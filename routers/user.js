8a7a8e6d6a4a524af7bb36b2747c82d93ca3d879
CH_Hwang <hch950627@naver.com>
2021-10-18 16:57:50 +0900

    [feat]회원가입 기능 추가

--- routers/user.js
+++ routers/user.js
@@ -2,10 +2,30 @@ const express = require('express');
 const router = express.Router();
 const { signupSchema } = require('./joi');
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
+const saltRound = 10;
+const { signupValidation } = require('./controller/signupValidation');
 
 router.post('/signup', async (req, res, next) => {
-  const { email, password, age } = signupSchema.validateAsync(req.body);
-  const nickname = email.split('@')[0].slice(0, 2);
+  try {
+    const { email, password, age } = signupSchema.validateAsync(req.body);
+    const nickname = email.substr(0, 3);
+    const hashedPassword = await bcrypt.hash(password, saltRound);
+    if (!signupValidation(email, password)) {
+      return res.sendStatus(400);
+    } else {
+      await User.create({
+        email,
+        password: hashedPassword,
+        age,
+        nickname,
+      });
+      return res.sendStatus(200);
+    }
+  } catch (err) {
+    //joi 벨리데이션, db검색 에러 둘중 하나가 잡힐것으로 예상됨
+    console.error(err);
+  }
 });
 
 module.exports = router;
