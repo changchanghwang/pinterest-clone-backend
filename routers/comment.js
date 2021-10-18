a23a92aaaaeeb22a141f494a0bd12f2de355aa23
CH_Hwang <hch950627@naver.com>
2021-10-18 20:26:11 +0900

    [feat] api 기본 구현 완료 (수정필요, 로그인인증기능 반영x)

--- routers/comment.js
+++ routers/comment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Comment } = require('../models');
+const { Comment, Like, Pin } = require('../models');
 
 router.post('/:pin', async (req, res, next) => {
   const { content } = req.body;
@@ -27,6 +27,26 @@ router.patch('/:comment', async (req, res, next) => {
     } else {
       await Comment.update({ content }, { where: { id: comment } });
       return res.sendStatus(200);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.post('/like/:pin', async (req, res, next) => {
+  const { pin } = req.params;
+  try {
+    const likeExist = await Like.findOne({ where: { pin } });
+    if (!likeExist) {
+      await Like.create({ pin });
+      const likeNum = await Like.count({ where: { pin } });
+      await Pin.update({ likeNum }, { where: { id: pin } });
+      res.sendStatus(200);
+    } else {
+      await Like.destroy({ where: { pin } });
+      const likeNum = await Like.count({ where: { pin } });
+      await Pin.update({ likeNum }, { where: { id: pin } });
+      res.sendStatus(200);
     }
   } catch (err) {
     console.error(err);
--- routers/view.js
+++ routers/view.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { Board, Pin } = require('../models');
+
+router.get('/submit', async (req, res, next) => {
+  const boards = await Board.findAll({});
+  res.status(200).json({ boards });
+});
+
+router.get('/my', async (req, res, next) => {
+  const myBoards = await Board.findAll({
+    include: [
+      {
+        model: Pin,
+        attributes: ['imgURL'],
+      },
+    ],
+    where: { user },
+  });
+  res.status(200).json({ myBoards });
+});
+
+router.get('/my/board/:board', async (req, res, next) => {
+  const { board } = req.params;
+  const pins = await Pin.findAll({ where: { board } });
+  res.status(200).json({ pins });
+});
+
 module.exports = router;
