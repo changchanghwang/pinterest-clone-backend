1b5213112471065d6c95f5836d0e67415011c7d4
CH_Hwang <hch950627@naver.com>
2021-10-18 16:30:38 +0900

    [feat] joi스키마 생성

--- routers/user.js
+++ routers/user.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { signupSchema } = require('./joi');
+const { User } = require('../models');
+
+router.post('/signup', async (req, res, next) => {
+  const { email, password, age } = signupSchema.validateAsync(req.body);
+  const nickname = email.split('@')[0].slice(0, 2);
+});
+
 module.exports = router;
