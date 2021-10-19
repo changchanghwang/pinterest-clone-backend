8cf05eaec4af5a29d0ccd56f8004e24ffe254672
changwoo Jeong <jcw9828@gmail.com>
2021-10-19 21:20:43 +0900

    [feat]핀등록

--- routers/pin.js
+++ routers/pin.js
@@ -2,4 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { Board, Pin } = require('../models');
 
+//핀 등록
+router.post('/', async (req, res) => {
+  const { title, desc, imgURL, boardId } = req.body;
+  const user = res.locals.user;
+  try {
+    await Pin.create({ title, desc, imgURL, boardId, user });
+    return res.status(200).send();
+  } catch (err) {
+    res.status(401).send(err);
+  }
+});
+
 module.exports = router;
