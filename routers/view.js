f372cec39d01228b0abfb16093913fa918223521
changwoo Jeong <jcw9828@gmail.com>
2021-10-19 09:52:21 +0900

    [feat]로그인 기능

--- routers/view.js
+++ routers/view.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Pin = require('../models/pins');
+
+/* 로그인 페이지 불러오기 */
+router.get('/login', async (req, res) => {
+  const { imgURL } = req.body;
+  try {
+    const img = await Pin.findAll({ where: { imgURL } });
+    console.log(img.imgURL);
+
+    res.status(200).json({ imgs, msg: 'ok' });
+  } catch (error) {
+    res.status(401).json({ msg: 'err' });
+  }
+});
+
 module.exports = router;
