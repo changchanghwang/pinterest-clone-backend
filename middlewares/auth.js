358db603e797288f3442feaab2e5d0512fad05cc
CH_Hwang <hch950627@naver.com>
2021-10-21 01:06:00 +0900

    [feat] 검색기능 추가

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
   console.log(req.headers.authorization);
   const { authorization } = req.headers;
   const [Bearer, token] = authorization.split(' ');
-  if (token === undefined) {
+  if (token == 'undefined') {
     return res.status(401).json({});
   }
   try {
