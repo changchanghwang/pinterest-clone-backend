5da894c5472d525ea15ead95d53f12613c752d40
changwoo Jeong <jcw9828@gmail.com>
2021-10-20 10:39:33 +0900

    [feat]핀등록록re

--- routers/pin.js
+++ routers/pin.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
+const { Pin } = require('../models');
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
   const { title, desc, imgURL, boardId } = req.body;
+  const user = res.locals.user;
+  try {
+    await Pin.create({ title, desc, imgURL, boardId, user });
+    return res.status(200).send();
+  } catch (err) {
+    res.status(401).send(err);
+  }
 });
 
 module.exports = router;
